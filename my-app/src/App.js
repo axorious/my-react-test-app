70d3867ed782cfbd3d8aad1484a10dc70dd80143
axorious <makz-2001@mail.ru>
2024-12-09 22:53:36 +0300

    задание без использование JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,36 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	// Императивный стиль: вычисление текущего года
 	const currentYear = new Date().getFullYear();
 
-	// Декларативный стиль: описание того, что должно отображаться на странице
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>Current Year: {currentYear}</p>
-			</header>
-		</div>
+	return React.createElement(
+		'div',
+		{ className: 'App' },
+		React.createElement(
+			'header',
+			{ className: 'App-header' },
+			React.createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			React.createElement(
+				'p',
+				null,
+				'Edit ',
+				React.createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			React.createElement('p', null, 'Current Year: ', currentYear),
+		),
 	);
 }
 
